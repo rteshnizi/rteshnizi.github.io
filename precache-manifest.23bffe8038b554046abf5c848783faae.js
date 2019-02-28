3eada87593c80d8854d7a0de746674d81636c798
Travis CI (on behalf of Reza Teshnizi) <rht1369@yahoo.com>
2019-02-28 06:01:29 +0000

    Travis build[19] --> fix(push): UNFUCK THE FORCE PUSH

---
+++ precache-manifest.23bffe8038b554046abf5c848783faae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "466b8c90db03df4b6a8d",
+    "url": "/static/js/main.466b8c90.chunk.js"
+  },
+  {
+    "revision": "6c1b6d4edc2e63ef86f9",
+    "url": "/static/js/2.6c1b6d4e.chunk.js"
+  },
+  {
+    "revision": "466b8c90db03df4b6a8d",
+    "url": "/static/css/main.0c920b70.chunk.css"
+  },
+  {
+    "revision": "65782db40c0995227dfa52a53a0670a9",
+    "url": "/index.html"
+  }
+];
